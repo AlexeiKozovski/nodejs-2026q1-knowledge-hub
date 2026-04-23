c8853e9b7bfb319d85c4369ebcc1822078b3e3cf
Alexey Kozovski <alexey.kozovski@idfinance.com>
2026-04-23 23:35:56 +0300

    refactor: update tests from template

--- test/utils/tokens.ts
+++ test/utils/tokens.ts
@@ -1,8 +1,7 @@
 import { sign, SignOptions } from 'jsonwebtoken';
 import 'dotenv/config';
 
-const refreshTokenSecurityKey =
-  process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET_REFRESH_KEY || '';
+const refreshTokenSecurityKey = process.env.JWT_SECRET_REFRESH_KEY || '';
 
 const generateRefreshToken = (payload: any, options: SignOptions): string => {
   return sign(payload, refreshTokenSecurityKey, options);
